ebaedfff6561ec31d488b44172396365b51ed9b3
vagrant <vagrant@vagrant.vm>
2018-11-12 22:19:11 +0000

    updates for Question 1 and Question 2

--- FSND_Logs.py
+++ FSND_Logs.py
@@ -3,52 +3,46 @@ import psycopg2
 #1. What are the most popular three articles of all time?
 #Example:
 
-#  "Princess Shellfish Marries Prince Handsome" — 1201 views
-#  "Baltimore Ravens Defeat Rhode Island Shoggoths" — 915 views
-#  "Political Scandal Ends In Political Scandal" — 553 views
+#  Princess Shellfish Marries Prince Handsome" 1201 views
+#  Baltimore Ravens Defeat Rhode Island Shoggoths 915 views
+#  Political Scandal Ends In Political Scandal 553 views
 
 
 #2. Who are the most popular article authors of all time?
 #Example:
 
-#  Ursula La Multa — 2304 views
-#  Rudolf von Treppenwitz — 1985 views
-#  Markoff Chaney — 1723 views
-#  Anonymous Contributor — 1023 views
+#  Ursula La Multa 2304 views
+#  Rudolf von Treppenwitz 1985 views
+#  Markoff Chaney  1723 views
+#  Anonymous Contributor 1023 views
 
 
 #3. On which days did more than 1% of requests lead to errors?
 
-#  July 29, 2016 — 2.5% errors
+#  July 29, 2016 2.5% errors
 
-#NOTES:
-#select select replace(log.path,'/article/','') as path
-dbname = news
+dbname = "news"
 
 db = psycopg2.connect(database=dbname)
 c = db.cursor()
-c.execute("create view newlog as
-select replace(log.path, '/article/','') as newpath, log.time, log.status,
-from log;")
-c.fetchall()
+#Creating View that preps log table into joinable format and excludes extra data.
+c.execute("create view newlog as select replace(path, '/article/','') as newpath, date(time) as newdate, status from log where path != '/' and status = '200 OK';")
+
+#Queary that joins newlog view and articles table for Question 1 and stores in ans1
+c.execute("Select articles.title, count(*) as num from newlog join articles on articles.slug = newlog.newpath group by articles.title order by num desc;")
+ans1 = c.fetchall()
+
+
+i = 0
+print("1. Top Most Popular Articles of all time are:")
+while i < 3:
+    print("    {} -- {} views".format(ans1[i][0],ans1[i][1]))
+    i += 1
+
+c.execute("Select newauthor.name, count(*) as num from newlog join (select authors.name, articles.slug from authors join articles on authors.id = articles.author) as newauthor on newauthor.slug = newlog.newpath group by newauthor.name order by num desc;")
+ans2 = c.fetchall()
+print(ans2)
+
 db.close()
 
 
-"""
-def get_posts():
-
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute("select content, time from posts order by time desc")
-  posts = c.fetchall()
-  db.close()
-  return posts
-
-def add_post(content):
-
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute("insert into posts values (%s)", (bleach.clean(content),))  # good
-  db.commit()
-  db.close()
-"""
